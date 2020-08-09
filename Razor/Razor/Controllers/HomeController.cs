21a0f1e873be8dc13bce6c612c0e4c3339101e3a
DavitMatevosyan <davitmatevosyan660@gmail.com>
2020-08-09 14:38:34 +0400

    Ch3

---
+++ Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Product[] array =
+            {
+                new Product {Name = "Kayak", Price = 275M },
+                new Product {Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Soccer Ball", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 94.95M}
+            };
+
+            return View(array);
+        }
+
+    //    public ViewResult Index()
+    //    {
+    //        Product myProduct = new Product() { ProductId = 1, Name = "Kayak", Description = "A Boat for one person", Category = "Watersports", Price = 275M };
+    //        ViewBag.StockLevel = 2;
+    //
+    //        return View(myProduct);
+    //    }
+    }
+}
